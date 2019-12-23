309a7bfe7c62825fb10416c8d13e1f0adae3702c
Gopinath <41997600+Gopilagadapati@users.noreply.github.com>
2019-12-23 21:07:25 +0530

    uploaded local

---
+++ create.py
@@ -0,0 +1,147 @@
+import json
+import logging
+import os
+import time
+import uuid
+from datetime import datetime
+
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+
+def create(event, context):
+    data = json.loads(event['body'])
+    if 'text' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't create the mentis item.")
+    
+    timestamp = str(datetime.utcnow().timestamp())
+
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    item = {
+        'id': str(uuid.uuid1()),
+        'text': data['text'],
+        'checked': False,
+        'createdAt': timestamp,
+        'updatedAt': timestamp,
+    }
+
+    # write the mentis to the database
+    table.put_item(Item=item)
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(item)
+    }
+
+    return response
+	
+	
+	
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return int(obj)
+        return super(DecimalEncoder, self).default(obj)
+
+
+
+def delete(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # delete the mentis from the database
+    table.delete_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    # create a response
+    response = {
+        "statusCode": 200
+    }
+
+    return response
+
+
+
+def get(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch mentis from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Item'],
+                           cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
+
+
+
+
+def list(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch all mentiss from the database
+    result = table.scan()
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
+
+
+
+
+
+def update(event, context):
+    data = json.loads(event['body'])
+    if 'text' not in data or 'checked' not in data:
+        logging.error("Validation Failed")
+        raise Exception("Couldn't update the mentis item.")
+        return
+
+    timestamp = int(time.time() * 1000)
+
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # update the mentis in the database
+    result = table.update_item(
+        Key={
+            'id': event['pathParameters']['id']
+        },
+        ExpressionAttributeNames={
+          '#mentis_text': 'text',
+        },
+        ExpressionAttributeValues={
+          ':text': data['text'],
+          ':checked': data['checked'],
+          ':updatedAt': timestamp,
+        },
+        UpdateExpression='SET #mentis_text = :text, '
+                         'checked = :checked, '
+                         'updatedAt = :updatedAt',
+        ReturnValues='ALL_NEW',
+    )
+
+    # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result['Attributes'],
+                           cls=decimalencoder.DecimalEncoder)
+    }
+
+    return response
